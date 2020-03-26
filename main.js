ac81ed37d25bc66689bff774c0c537e34743574e
olusegz95 <38265905+olusegz95@users.noreply.github.com>
2020-03-26 17:46:31 +0100

    javascript Task1
    First Javascript Task

---
+++ main.js
@@ -0,0 +1,12 @@
+//Variables
+const name = "Olusegun Faulkner";
+console.log(name);
+//Arrays
+const courses = [ 'HTML','CSS','JAVASCRIPT','UI/UX'];
+console.log(courses);
+// Javascript loops
+for (let i=1; i<=200; i++) {
+	if (i % 2 ==0) console.log(i);
+} 
+
+
